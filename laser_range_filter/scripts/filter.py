60db637dc9de90c95379e50b03fee9c8be4f581f
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2015-10-12 20:10:45 +0200

    in laser range filter also filter away closeby points:
    laser scanners have a very bad habit of extending the obstacles into space as if they were cones and not points, therefore, try to cut out the cones as well

--- laser_range_filter/scripts/filter.py
+++ laser_range_filter/scripts/filter.py
@@ -14,12 +14,24 @@ class LaserRangeFilter:
         filtered_ranges = []
         filtered_intensities = []
         for rng, intensity in zip(scan_msg.ranges, scan_msg.intensities):
-            if (rng > 0.4):
+            if (rng > 0.3):
                 filtered_ranges.append(rng)
                 filtered_intensities.append(intensity)
             else:
-               filtered_ranges.append(0)
+               filtered_ranges.append(111)
                filtered_intensities.append(0)
+        margin = 19
+        inside_bad_range_flag = 0
+        for i in range(margin, len(filtered_ranges) - margin):
+            if (filtered_ranges[i] == 111):
+                inside_bad_range_flag = margin
+                for j in range(i - margin, i - 1):
+                    filtered_ranges[j] = 111
+                    filtered_intensities[j] = 0
+            elif (inside_bad_range_flag > 0):
+                inside_bad_range_flag -= 1
+                filtered_ranges[i] = 111
+                filtered_intensities[i] = 0
         scan_msg.ranges = filtered_ranges
         scan_msg.intensities = filtered_intensities
         self.pub.publish(scan_msg)
